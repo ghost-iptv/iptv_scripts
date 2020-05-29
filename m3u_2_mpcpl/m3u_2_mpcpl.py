df9964e0535291f27ca720b63ead798ff1163789
Ghost IPTV <9326478+ghost-iptv@users.noreply.github.com>
2020-05-29 23:48:10 +0200

    Update m3u_2_mpcpl.py
    Converting Playlists to MPCPLAYLIST [.mpcpl] Format

--- m3u_2_mpcpl/m3u_2_mpcpl.py
+++ m3u_2_mpcpl/m3u_2_mpcpl.py
@@ -5,5 +5,5 @@ for file in os.listdir(os.getcwd()):
         with open(file.rsplit('.', 1)[0]+'.mpcpl', 'w+') as f:
             f.write('MPCPLAYLIST\n')
             for i in range(0, len(m3u)):
-                if m3u[i].startswith(('http://', 'https://', 'file://', 'rtsp://', 'rtmp://', 'mms://', 'ftp://', 'udp://')):
+                if m3u[i].startswith(('http://', 'https://', 'file://', 'rtsp://', 'rtmp://', 'mms://', 'ftp://', 'udp://')) :
                     f.write('{i},type,0\n{i},label,{name}\n{i},filename,{link}\n'.format(i=i//2, name=m3u[i-1].split(',', 1)[1].strip(), link=m3u[i]))
